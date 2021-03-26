78b9f7cbec344e40b63a8ec7594c4becfba4b0a1
Sean Ottomanelli <sean.ottomanelli@gmail.com>
2021-03-26 16:55:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const idMain = document.getElementById('main');
+idMain.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Sean is the champion"
